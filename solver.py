3dcbe6d057deeebfe42ff284b0e0ee00f6c180d7
Anup Agarwal <anupa@andrew.cmu.edu>
2019-11-19 21:43:19 -0700

    Fix mem bugs and pretty intuitive now

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 import pprint
+import math
 
 def ap(first, last, step=1):
     return [first + x*step for x in range(int((last - first)/step + 1))]
@@ -17,7 +18,7 @@ M_cpu_range = [10, 32, 80, 160, 240, 256, 512, 1024, 2048, 4096, 8192, 16384, 32
 r_cpu_range = ap(4, 9)
 ranges = {
     'f': ap(0.01, 0.99, 0.05),
-    'M_p4': [2048, 4096, 8192, 16384, 65536, 131072],
+    'k_p4': [2048, 4096, 8192, 16384, 65536, 131072],
     'r_p4': ap(4, 9),
     'M_cpu': M_cpu_range,
     'r_cpu': r_cpu_range,
@@ -27,10 +28,11 @@ ranges = {
 }
 
 keys = list(ranges.keys())
-print(keys)
+# print(keys)
 
 # CPU Model
 # For dpdk
+# TODO:: replace with OVS rate
 fraction_parallel = 3/4 # Amdahls law
 single_core_thr = 35
 
@@ -38,12 +40,12 @@ single_core_thr = 35
 # in KB
 L1_size = 32
 L2_size = 256
-L3_size = 8192
+L3_size = 7775 #8192
 
 L1_ns = 0.6
 L2_ns = 1.5
 L3_ns = 3.7
-L4_ns = 31
+L4_ns = 36
 hash_ns = 3.5
 
 static_loads = [18, 24, 30, 43, 49, 55]
@@ -57,30 +59,32 @@ def get_ns_per_packet(r, M, sketch_cores):
 
     ns_per_packet = hash_ns * r + (t + r1) * L1_ns + r2 * L2_ns + r3 * L3_ns + r4 * L4_ns;
     ns_per_packet = ns_per_packet / sketch_cores
-    return ns_per_packet
+    return ns_per_packet #, t + r1 + r2 + r3 + r4, r2 + r3 + r4, r3 + r4, r4
 
 # P4 Model
 # in Mpps (if sketch fits then line rate)
 throughput_p4 = 148
 
 # Constraints
-epsilon_0 = 0.0001
+epsilon_0 = 0.00001
 
 # Optimum leeway (relative)
 thr_leeway = 0.05
-resource_leeway = 0.05
+resource_leeway = 0.0005
 
 max_thr = -1
-solutions = []
+solution = []
 def solve(selection, key):
     if key < len(keys):
         for i in ranges[keys[key]]:
             selection[keys[key]] = i
             solve(selection, key + 1)
     else:
-        s = selection
+        s = selection.copy()
+        k_p4 = s['k_p4']
+        s['M_p4'] = k_p4 * 4 * s['r_p4'] / 1024
         k_cpu = s['M_cpu'] * 1024 / (4 * s['r_cpu'])
-        k_p4 = s['M_p4'] * 1024 / (4 * s['r_p4'])
+        #k_p4 = s['M_p4'] * 1024 / (4 * s['r_p4'])
         epsilon_p4 = 2 / k_p4
         epsilon_cpu = 2 / k_cpu
         f = s['f']
@@ -122,22 +126,26 @@ def solve(selection, key):
         global max_thr
         global solution
 
-        sol = selection.copy()
-        sol['ns_per_packet_single_core'] = ns_per_packet * sketch_cores
-        sol['ns_per_packet'] = ns_per_packet
-        sol['throughput'] = throughput
-        sol['sketch_cores'] = sketch_cores
+        s['ns_per_packet_single_core'] = ns_per_packet * sketch_cores
+        s['ns_per_packet'] = ns_per_packet
+        s['throughput'] = throughput
+        s['sketch_cores'] = sketch_cores
+        s['num_col_bits_p4'] = math.log(s['k_p4'], 2)
+        s['eps_p4'] = epsilon_p4
+        s['eps_cpu'] = epsilon_cpu
+        s['k_cpu'] = k_cpu
 
         if(throughput > max_thr):
             max_thr = throughput
-            solution = [sol]
+            solution = [s]
         else:
+            # TODO::
             # Fix this: prior solutions could also be within 10% of new max
             if((max_thr - throughput) / max_thr < thr_leeway):
-                solution.append(sol)
+                solution.append(s)
         return
 
-'''
+
 solve({}, 0)
 #pprint.pprint(solution)
 print(max_thr, len(solution))
@@ -145,20 +153,28 @@ print(max_thr, len(solution))
 min_obj = 1e9
 reduced_solution = []
 for s in solution:
-    obj = s['cpu_cores']
-    obj += s['M_p4']/32768 # num stages due to cols
-    obj += s['r_p4']/4 # num stages due to rows
+    obj = 10*s['cpu_cores'] # Weigh cores 10 times more than stages
+    obj += s['M_cpu'] / (256) # fit in L2
+    obj += abs(s['num_col_bits_p4'] - 15) # num stages due to cols
+    obj += math.ceil(s['r_p4']/4) # num stages due to rows
 
     s['obj'] = obj
     if (obj < min_obj):
         reduced_solution = [s]
         min_obj = obj
     else:
+        #TODO:: prior can also be in this
         if(obj - min_obj)/min_obj < resource_leeway:
             reduced_solution.append(s)
 
 print(min_obj, len(reduced_solution))
 pprint.pprint(reduced_solution)
-'''
 
+
+'''
 # Test CPU Model
+for r in r_cpu_range:
+    for M in M_cpu_range:
+        ns, l1, l2, l3, l4 = get_ns_per_packet(r, M, 1)
+        print("{}, {}, {}, {}, {}, {}, {}".format(r, M, ns, l1, l2, l3, l4))
+'''
