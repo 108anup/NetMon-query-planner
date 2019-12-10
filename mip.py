323f6170b541b33be40af280ac9e336d49117e83
Anup Agarwal <108anup@gmail.com>
2019-12-10 16:52:59 -0500

    fix log precision error

--- mip.py
+++ mip.py
@@ -46,29 +46,53 @@ class cpu(param):
     # TODO:: update with OVS
     fraction_parallel = 3/4
 
+    def get_pdt_var(self, a, b, pdt_name, m):
+        m.update()
+        # print(a)
+        # print(a.varName)
+        # ipdb.set_trace()
+        pdt = m.addVar(vtype=GRB.CONTINUOUS,
+                       name='pdt_{}_{}'.format(pdt_name, self))
+        loga = m.addVar(vtype=GRB.CONTINUOUS,
+                        name='log_{}'.format(a.varName))
+        logb = m.addVar(vtype=GRB.CONTINUOUS,
+                        name='log_{}'.format(b.varName))
+        logpdt = m.addVar(vtype=GRB.CONTINUOUS,
+                          name='log_pdt_{}_{}'.format(pdt_name, self))
+        # m.addGenConstrExpA(logpdt, pdt, 2,
+        #                    name='exp_pdt_{}_{}'.format(pdt_name, self))
+        m.addGenConstrLogA(pdt, logpdt, 2,
+                           name='log_pdt_{}_{}'.format(pdt_name, self),
+                           options="FuncPieces=-1 FuncPieceError=0.001")
+        m.addGenConstrLogA(a, loga, 2,
+                           name='log_{}'.format(a.varName),
+                           options="FuncPieces=-1 FuncPieceError=0.001")
+        m.addGenConstrLogA(b, logb, 2,
+                           name='log_{}'.format(b.varName),
+                           options="FuncPieces=-1 FuncPieceError=0.001")
+        m.addConstr(logpdt == loga + logb,
+                    name='pdt_{}_{}'.format(pdt_name, self))
+        return pdt
+
     def update_ns(self, rows, mem, m):
+        # Access time based on mem
         # TODO:: better fits for mem and t
-        t = 18 + (rows-4)*6 - rows
         self.m_access_time = m.addVar(vtype=GRB.CONTINUOUS, lb=0,
                                       name='m_access_time_{}'.format(self))
         m.addGenConstrPWL(mem, self.m_access_time, self.mem_par, self.mem_ns,
                           "mem_access_time_{}".format(self))
+
+        # single core ns model
+        t = 18 + (rows-4)*6 - rows
         self.ns_single = m.addVar(vtype=GRB.CONTINUOUS,
                                   name='ns_single_{}'.format(self))
-        log_m_access_time = m.addVar(vtype=GRB.CONTINUOUS)
-        log_rows = m.addVar(vtype=GRB.CONTINUOUS)
-        log_pdt_m_row = m.addVar(vtype=GRB.CONTINUOUS)
-        pdt_m_row = m.addVar(vtype=GRB.CONTINUOUS)
+        self.pdt_m_rows = self.get_pdt_var(self.m_access_time,
+                                           rows, 'm_rows', m)
+        m.addConstr(t * self.mem_ns[0] + self.pdt_m_rows
+                    + rows * self.hash_ns
+                    == self.ns_single, name='ns_single_{}'.format(self))
 
-        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
-        #              + rows * self.hash_ns
-        #              - self.ns_single <= invtol,
-        #              name='ns_u_single_{}'.format(self))
-        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
-        #              + rows * self.hash_ns
-        #              - self.ns_single >= -invtol,
-        #              name='ns_l_single_{}'.format(self))
-        
+        # Multi-core model
         self.cores_sketch = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores,
                                      name='cores_sketch_{}'.format(self))
         self.cores_dpdk = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores,
@@ -76,27 +100,26 @@ class cpu(param):
         m.addConstr(self.cores_sketch + self.cores_dpdk <= self.cores,
                     name='capacity_cores_{}'.format(self))
         self.ns_sketch = m.addVar(vtype=GRB.CONTINUOUS,
-                                  name='ns_sketch_{}'.format(self))
+                                  name='ns_sketch_{}'.format(self), lb=0)
         self.ns_dpdk = m.addVar(vtype=GRB.CONTINUOUS,
                                 name='ns_dpdk_{}'.format(self))
         self.ns = m.addVar(vtype=GRB.CONTINUOUS,
                            name='ns_{}'.format(self))
-        # m.addQConstr(self.ns_sketch*self.cores_sketch
-        #              - self.ns_single <= invtol,
-        #              name='ns_u_sketch_{}'.format(self))
-        # m.addQConstr(self.ns_sketch*self.cores_sketch
-        #              - self.ns_single >= invtol,
-        #              name='ns_l_sketch_{}'.format(self))
-        # m.addQConstr(
-        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
-        #     * self.ns_dpdk
-        #     - self.dpdk_single_core_thr * self.cores_dpdk <= invtol,
-        #     name='ns_u_dpdk_{}'.format(self))
-        # m.addQConstr(
-        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
-        #     * self.ns_dpdk
-        #     - self.dpdk_single_core_thr * self.cores_dpdk >= -invtol,
-        #     name='ns_l_dpdk_{}'.format(self))
+
+        # Multi-core sketching
+        self.pdt_nsk_c = self.get_pdt_var(self.ns_sketch,
+                                          self.cores_sketch, 'nsk_c', m)
+        m.addConstr(self.pdt_nsk_c == self.ns_single,
+                    name='ns_sketch_{}'.format(self))
+
+        # Amdahl's law
+        self.dpdk_single_ns = 1000/self.dpdk_single_core_thr
+        self.pdt_nsc_dpdk = self.get_pdt_var(
+            self.ns_dpdk, self.cores_dpdk, 'nsc_dpdk', m)
+        m.addConstr(
+            (self.cores_dpdk*(1-cpu.fraction_parallel)
+             + cpu.fraction_parallel)*self.dpdk_single_ns
+            == self.pdt_nsc_dpdk, name='ns_dpdk_{}'.format(self))
         m.addGenConstrMax(self.ns, [self.ns_dpdk, self.ns_sketch],
                           name='ns_{}'.format(self))
 
@@ -136,8 +159,8 @@ queries = [cm_sketch(eps0=eps0*50, del0=del0),
 partitions = []
 for (i, q) in enumerate(queries, 1):
     q.sketch_id = i
-    rows = q.rows()
-    partitions += [(r+1, q) for r in range(rows)]
+    num_rows = q.rows()
+    partitions += [(r+1, q) for r in range(num_rows)]
 
 # All memory measured in KB unless otherwise specified
 devices = [
@@ -160,7 +183,7 @@ frac = m.addVars(numdevices, numpartitions, vtype=GRB.CONTINUOUS,
 mem = m.addVars(numdevices, numpartitions, vtype=GRB.CONTINUOUS,
                 lb=0, name='mem')
 rows = m.addVars(numdevices, numpartitions, vtype=GRB.BINARY,
-                 name='rows')
+                 name='rows', lb=0)
 m.addConstrs((rows[i, j] >= frac[i, j]
               for i in range(numdevices)
               for j in range(numpartitions)), name='r_ceil0')
@@ -168,6 +191,9 @@ m.addConstrs((rows[i, j] >= frac[i, j]
 m.addConstrs((rows[i, j] <= frac[i, j] + tolerance
               for i in range(numdevices)
               for j in range(numpartitions)), name='r_ceil1')
+for pnum in range(numpartitions):
+    m.addConstr(frac.sum('*', pnum) == 1,
+                name='cov_{}'.format(pnum))
 
 # Accuracy constraints
 for (pnum, p) in enumerate(partitions):
@@ -228,3 +254,55 @@ print("Model optimize took: {} seconds".format(end - start))
 
 for v in m.getVars():
     print('%s %g' % (v.varName, v.x))
+
+
+
+'''
+Extra Codes
+
+        # self.log_m_access_time = m.addVar(vtype=GRB.CONTINUOUS,
+        #                                   name='log_m_access_time_{}'.format(self))
+        # self.log_rows = m.addVar(vtype=GRB.CONTINUOUS,
+        #                          name='log_rows_{}'.format(self))
+        # self.log_pdt_m_rows = m.addVar(vtype=GRB.CONTINUOUS,
+        #                                name='log_pdt_m_rows{}'.format(self))
+        # self.pdt_m_rows = m.addVar(vtype=GRB.CONTINUOUS,
+        #                            name='pdt_m_rows_{}'.format(self))
+        # m.addGenConstrLogA(self.m_access_time, self.log_m_access_time, 2,
+        #                    name='log_mem_{}'.format(self))
+        # m.addGenConstrLogA(rows, self.log_rows, 2,
+        #                    name='log_rows_{}'.format(self))
+        # m.addGenConstrLogA(self.pdt_m_rows, self.log_pdt_m_rows, 2,
+        #                    name='log_pdt_m_rows_{}'.format(self))
+
+
+        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
+        #              + rows * self.hash_ns
+        #              - self.ns_single <= invtol,
+        #              name='ns_u_single_{}'.format(self))
+        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
+        #              + rows * self.hash_ns
+        #              - self.ns_single >= -invtol,
+        #              name='ns_l_single_{}'.format(self))
+
+        # m.addQConstr(self.ns_sketch*self.cores_sketch
+        #              - self.ns_single <= invtol,
+        #              name='ns_u_sketch_{}'.format(self))
+        # m.addQConstr(self.ns_sketch*self.cores_sketch
+        #              - self.ns_single >= invtol,
+        #              name='ns_l_sketch_{}'.format(self))
+
+
+        # m.addQConstr(
+        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
+        #     * self.ns_dpdk
+        #     - self.dpdk_single_core_thr * self.cores_dpdk <= invtol,
+        #     name='ns_u_dpdk_{}'.format(self))
+        # m.addQConstr(
+        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
+        #     * self.ns_dpdk
+        #     - self.dpdk_single_core_thr * self.cores_dpdk >= -invtol,
+        #     name='ns_l_dpdk_{}'.format(self))
+
+
+'''
