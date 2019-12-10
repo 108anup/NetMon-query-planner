ca48d48af184a035777873473dd5d21c5a804cee
Anup Agarwal <108anup@gmail.com>
2019-12-10 14:43:45 -0500

    Add obj to mip

--- mip.py
+++ mip.py
@@ -1,7 +1,8 @@
 import gurobipy as gp
 from gurobipy import GRB
 import math
-
+import time
+import ipdb
 
 def memoize(func):
     cache = dict()
@@ -27,6 +28,7 @@ class param:
 class cm_sketch(param):
 
     def __repr__(self):
+        return 'sk_{}'.format(self.sketch_id)
         return "cm: eps0: {}, del0: {}".format(self.eps0, self.del0)
 
     def rows(self):
@@ -44,42 +46,77 @@ class cpu(param):
     # TODO:: update with OVS
     fraction_parallel = 3/4
 
-    def ns(self, rows, mem, m):
+    def update_ns(self, rows, mem, m):
         # TODO:: better fits for mem and t
         t = 18 + (rows-4)*6 - rows
-        self.m_access_time = m.addVar(vtype=GRB.CONTINUOUS, lb=0)
-        m.addGenConstrPWL(mem, self.m_access_time, self.mem_par, self.mem_ns)
-        self.ns_single = m.addVar(vtype=GRB.CONTINUOUS)
-        m.addConstr(self.ns_single == t * self.mem_ns[0]
-                    + self.m_access_time * rows + rows * self.hash_ns)
-        self.cores_sketch = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores)
-        self.cores_dpdk = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores)
-        m.addConstr(self.cores.sketch + self.cores.dpdk <= self.cores)
-        self.ns_sketch = m.addVar(vtype=GRB.CONTINUOUS)
-        self.ns_dpdk = m.addVar(vtype=GRB.CONTINUOUS)
-        self.ns = m.addVar(vtype=GRB.CONTINUOUS)
-        m.addConstr(self.ns_sketch*self.cores_sketch == self.ns_single)
-        m.addConstr(
-            ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
-            * self.ns_dpdk == self.dpdk_single_core_thr * self.cores_dpdk)
-        m.addConstr(self.ns == gp.min_(self.ns_dpdk, self.ns_sketch))
+        self.m_access_time = m.addVar(vtype=GRB.CONTINUOUS, lb=0,
+                                      name='m_access_time_{}'.format(self))
+        m.addGenConstrPWL(mem, self.m_access_time, self.mem_par, self.mem_ns,
+                          "mem_access_time_{}".format(self))
+        self.ns_single = m.addVar(vtype=GRB.CONTINUOUS,
+                                  name='ns_single_{}'.format(self))
+        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
+        #              + rows * self.hash_ns
+        #              - self.ns_single <= invtol,
+        #              name='ns_u_single_{}'.format(self))
+        # m.addQConstr(t * self.mem_ns[0] + self.m_access_time * rows
+        #              + rows * self.hash_ns
+        #              - self.ns_single >= -invtol,
+        #              name='ns_l_single_{}'.format(self))
+        self.cores_sketch = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores,
+                                     name='cores_sketch_{}'.format(self))
+        self.cores_dpdk = m.addVar(vtype=GRB.INTEGER, lb=1, ub=self.cores,
+                                   name='cores_dpdk_{}'.format(self))
+        m.addConstr(self.cores_sketch + self.cores_dpdk <= self.cores,
+                    name='capacity_cores_{}'.format(self))
+        self.ns_sketch = m.addVar(vtype=GRB.CONTINUOUS,
+                                  name='ns_sketch_{}'.format(self))
+        self.ns_dpdk = m.addVar(vtype=GRB.CONTINUOUS,
+                                name='ns_dpdk_{}'.format(self))
+        self.ns = m.addVar(vtype=GRB.CONTINUOUS,
+                           name='ns_{}'.format(self))
+        # m.addQConstr(self.ns_sketch*self.cores_sketch
+        #              - self.ns_single <= invtol,
+        #              name='ns_u_sketch_{}'.format(self))
+        # m.addQConstr(self.ns_sketch*self.cores_sketch
+        #              - self.ns_single >= invtol,
+        #              name='ns_l_sketch_{}'.format(self))
+        # m.addQConstr(
+        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
+        #     * self.ns_dpdk
+        #     - self.dpdk_single_core_thr * self.cores_dpdk <= invtol,
+        #     name='ns_u_dpdk_{}'.format(self))
+        # m.addQConstr(
+        #     ((1-cpu.fraction_parallel)*self.cores_dpdk + cpu.fraction_parallel)
+        #     * self.ns_dpdk
+        #     - self.dpdk_single_core_thr * self.cores_dpdk >= -invtol,
+        #     name='ns_l_dpdk_{}'.format(self))
+        m.addGenConstrMax(self.ns, [self.ns_dpdk, self.ns_sketch],
+                          name='ns_{}'.format(self))
 
     def res(self, rows, mem):
-        return self.cores_dpdk + self.cores_sketch
+        return 10*(self.cores_dpdk + self.cores_sketch)
+
+    def __repr__(self):
+        return self.name
 
 
 class p4(param):
 
-    def ns(self, rows, mem):
-        self.ns = m.addVar(vtype=GRB.CONTINUOUS)
-        m.addConstr(self.ns == 1000 / self.line_thr)
+    def update_ns(self, rows, mem, m):
+        self.ns = m.addVar(vtype=GRB.CONTINUOUS, name='ns_{}'.format(self))
+        m.addConstr(self.ns == 1000 / self.line_thr, name='ns_{}'.format(self))
 
     def res(self, rows, mem):
         return rows/self.meter_alus + mem/self.sram
 
+    def __repr__(self):
+        return self.name
+
 
 # Global Params
 tolerance = 0.999
+invtol = 1-tolerance
 cell_size = 4
 KB2B = 1024
 
@@ -101,71 +138,87 @@ devices = [
     cpu(mem_par=[1.1875, 32, 1448.15625, 5792.625, 32768.0, 440871.90625],
         mem_ns=[0.539759, 0.510892, 5.04469, 5.84114, 30.6627, 39.6981],
         hash_ns=3.5, cores=7, dpdk_single_core_thr=35,
-        max_mem=32768, max_rows=9),
+        max_mem=32768, max_rows=9, name='cpu_1'),
     p4(meter_alus=4, sram=48, stages=12, line_thr=148,
-       max_mpp=48, max_mem=48*12, max_rows=12)
+       max_mpp=48, max_mem=48*12, max_rows=12, name='p4_1')
 ]
+numdevices = len(devices)
+numpartitions = len(partitions)
 
 # Build model
 m = gp.Model('netmon')
 
 # Decl of vars
-frac = m.addVars(devices, partitions, vtype=GRB.CONTINUOUS, lb=0, ub=1)
-mem = m.addVars(devices, partitions, vtype=GRB.CONTINUOUS, lb=1)
-rows = m.addVars(devices, partitions, vtype=GRB.BINARY)
+frac = m.addVars(numdevices, numpartitions, vtype=GRB.CONTINUOUS,
+                 lb=0, ub=1, name='frac')
+mem = m.addVars(numdevices, numpartitions, vtype=GRB.CONTINUOUS,
+                lb=0, name='mem')
+rows = m.addVars(numdevices, numpartitions, vtype=GRB.BINARY,
+                 name='rows')
 m.addConstrs((rows[i, j] >= frac[i, j]
-              for i in range(len(devices))
-              for j in range(len(partitions))), name='r_ceil0')
+              for i in range(numdevices)
+              for j in range(numpartitions)), name='r_ceil0')
 
 m.addConstrs((rows[i, j] <= frac[i, j] + tolerance
-              for i in range(len(devices))
-              for j in range(len(partitions))), name='r_ceil1')
+              for i in range(numdevices)
+              for j in range(numpartitions)), name='r_ceil1')
 
 # Accuracy constraints
 for (pnum, p) in enumerate(partitions):
     sk = p[1]
     mm = sk.min_mem()
-    m.addConstrs((mem[pnum, d] >= mm * frac[pnum][d]
-                  for d in range(len(devices))), name='accuracy')
+    m.addConstrs((mem[dnum, pnum] >= mm * frac[dnum, pnum]
+                  for dnum in range(numdevices)), name='accuracy_{}'.format(p))
 
 # Capacity constraints and throughput
 resacc = gp.QuadExpr()
 for (dnum, d) in enumerate(devices):
     # Simple total model
-    rows_tot = rows.sum(dnum, '*')
-    mem_tot = mem.sum(dnum, '*')
+    rows_tot = m.addVar(vtype=GRB.INTEGER, name='rows_tot_{}'.format(d))
+    mem_tot = m.addVar(vtype=GRB.CONTINUOUS, name='mem_tot_{}'.format(d))
+    m.addConstr(rows_tot == rows.sum(dnum, '*'), name='rows_tot_{}'.format(d))
+    m.addConstr(mem_tot == mem.sum(dnum, '*'), name='mem_tot_{}'.format(d))
 
     # Capacity constraints
-    m.addConstrs(mem_tot <= d.max_mem)
-    m.addConstrs(rows_tot <= d.max_rows)
+    m.addConstr(mem_tot, GRB.LESS_EQUAL, d.max_mem,
+                'capacity_mem_tot_{}'.format(d))
+    m.addConstr(rows_tot, GRB.LESS_EQUAL, d.max_rows,
+                'capacity_compute_{}'.format(d))
 
     if isinstance(d, p4):
         for (pnum, p) in enumerate(partitions):
-            m.addConstrs(mem[dnum, pnum] < d.max_mpp)
+            m.addConstr(mem[dnum, pnum] <= d.max_mpp,
+                        'capacity_mem_par_{}'.format(d))
 
     # Throughput
     # NOTE: Following function updates m
-    d.ns(rows_tot, mem_tot, m)
+    d.update_ns(rows_tot, mem_tot, m)
 
     # Resources
-    resacc += d.res(m)
+    resacc += d.res(rows_tot, mem_tot)
 
 ns_series = [d.ns for d in devices]
-ns = m.addVars(vtype=GRB.CONTINUOUS)
-m.addConstr(ns == gp.min_(ns_series))
-res = m.addVars(vtype=GRB.CONTINUOUS)
-m.addConstr(res == resacc)
+ns = m.addVar(vtype=GRB.CONTINUOUS, name='ns')
+m.addGenConstrMax(ns, ns_series, name='ns_overall')
+res = m.addVar(vtype=GRB.CONTINUOUS, name='res_overall')
+m.addConstr(res == resacc, name='res')
 
 # TODO:: fill with multi objective
+m.ModelSense = GRB.MINIMIZE
+m.setObjectiveN(ns, 0, 10, reltol=0.01, name='ns')
+m.setObjectiveN(res, 1, 5, reltol=0.01, name='res')
 
+start = time.time()
+m.update()
+end = time.time()
+print("Model update took: {} seconds".format(end - start))
 
+m.write("prog.lp")
 
-m.setObjective(obj, GRB.MAXIMIZE)
+start = time.time()
 m.optimize()
+end = time.time()
+print("Model optimize took: {} seconds".format(end - start))
 
 for v in m.getVars():
     print('%s %g' % (v.varName, v.x))
-
-print('Obj: %g' % obj.getValue())
-
-
