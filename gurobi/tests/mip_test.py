ec21b2858067238ab839822f3b64a0ef760b3518
Anup Agarwal <108anup@gmail.com>
2019-12-12 17:51:31 -0500

    test product with zero terms

--- gurobi/tests/mip_test.py
+++ gurobi/tests/mip_test.py
@@ -8,17 +8,17 @@ def get_pdt(a, b, pdt_name, m):
     pdt = m.addVar(vtype=GRB.CONTINUOUS,
                    name='pdt_{}'.format(pdt_name))
     loga = m.addVar(vtype=GRB.CONTINUOUS,
-                    name='log_{}'.format(a.varName))
+                    name='log_{}'.format(a.varName), lb=-GRB.INFINITY)
     logb = m.addVar(vtype=GRB.CONTINUOUS,
-                    name='log_{}'.format(b.varName))
+                    name='log_{}'.format(b.varName), lb=-GRB.INFINITY)
     logpdt = m.addVar(vtype=GRB.CONTINUOUS,
-                      name='log_pdt_{}'.format(pdt_name))
-    m.addGenConstrLogA(pdt, logpdt, 2,
+                      name='log_pdt_{}'.format(pdt_name), lb=-GRB.INFINITY)
+    m.addGenConstrExpA(logpdt, pdt, 2,
                        name='log_pdt_{}'.format(pdt_name),
-                       options="FuncPieces=-1 FuncPieceError=0.001")
-    m.addGenConstrLogA(a, loga, 2,
+                       options="FuncPieces=-1 FuncPieceError=0.00001")
+    m.addGenConstrExpA(loga, a, 2,
                        name='log_{}'.format(a.varName))
-    m.addGenConstrLogA(b, logb, 2,
+    m.addGenConstrExpA(logb, b, 2,
                        name='log_{}'.format(b.varName))
     m.addConstr(logpdt == loga + logb,
                 name='pdt_{}'.format(pdt_name))
@@ -26,17 +26,17 @@ def get_pdt(a, b, pdt_name, m):
 
 
 m = gp.Model('pdt_test')
-x = m.addVar(vtype=GRB.CONTINUOUS, lb=5, ub=6, name='x')
-y = m.addVar(vtype=GRB.CONTINUOUS, lb=6, ub=7, name='y')
-z = m.addVar(vtype=GRB.CONTINUOUS, lb=11, ub=11, name='z')
-logz = m.addVar(vtype=GRB.CONTINUOUS, name='logz')
+x = m.addVar(vtype=GRB.CONTINUOUS, name='x')
+y = m.addVar(vtype=GRB.CONTINUOUS, name='y')
+z = m.addVar(vtype=GRB.CONTINUOUS, name='z')
+logz = m.addVar(vtype=GRB.CONTINUOUS, name='logz', lb=-1, ub=-0.5)
 ez = m.addVar(vtype=GRB.CONTINUOUS, name='ez')
 
 m.addGenConstrLogA(z, logz, 2)
 m.addGenConstrExpA(z, ez, 2)
 
 xy = get_pdt(x, y, 'xy', m)
-m.setObjective(x+y+xy, GRB.MINIMIZE)
+m.setObjective(x + y + xy, GRB.MINIMIZE)
 
 start = time.time()
 m.update()
@@ -51,4 +51,6 @@ end = time.time()
 print("Model optimize took: {} seconds".format(end - start))
 
 for v in m.getVars():
-    print('%s %g' % (v.varName, v.x))
+    print('{} {}'.format(v.varName, v.x))
+
+print("Obj: {}".format(m.getObjective().getValue()))
